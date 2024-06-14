14516f89779f0023565e1277671146553c1fbae6
Jasur0919 <jasurjohn1111@gmail.com>
2024-06-14 18:46:10 +0500

    API

---
+++ src/component/user/User.jsx
@@ -0,0 +1,21 @@
+import axios from 'axios'
+import React from 'react'
+import { useState, useEffect } from 'react'
+
+
+
+const User = () => {
+
+    useEffect(() => {
+        axios
+            .get
+    })
+  return (
+    <div>
+        <h1>User</h1>
+
+    </div>
+  )
+}
+
+export default User
